760bc69edcfd528cfde70d6d8513d823ed0884de
WilliamHammond <william.t.hammond@gmail.com>
2018-02-04 20:33:41 -0500

    use ubuntu, start basic webserver

--- main.tf
+++ main.tf
@@ -4,7 +4,49 @@ provider "aws" {
 	region = "${var.region}"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "site-host" {
-	ami           = "ami-2757f631"
+	ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+	key_name 			= "personal-site"
+
+	provisioner "file" {
+		source 			= "/home/whammond/go/bin/personal-site"
+		destination = "./personal-site"
+		
+		connection {
+			type        = "ssh"
+			user 			  = "ubuntu"
+			private_key = "${file("~/.ssh/personal-site.pem")}"
+		}
+	}
+
+	provisioner "remote-exec" {
+		inline = [
+			"sudo mv personal-site /bin/personal-site",
+			"chmod +x /bin/personal-site",
+			"personal-site &"	
+		]	
+
+		connection {
+			type        = "ssh"
+			user 			  = "ubuntu"
+			private_key = "${file("~/.ssh/personal-site.pem")}"
+		}
+	}
 }
