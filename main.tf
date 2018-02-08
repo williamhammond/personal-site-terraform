e449b820b0906c41fd1384a23e530de13c93bba2
WilliamHammond <william.t.hammond@gmail.com>
2018-02-07 21:40:59 -0500

    close down any sort of access to just my machine

--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ resource "aws_security_group" "personal-site" {
 		from_port   = 22
 		to_port     = 22
 		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
+		cidr_blocks = ["${var.my_ip}"]
 	}
 
 	egress {
-		from_port 	= 0
-		to_port 	  = 0
-		protocol  	= "-1"
-		cidr_blocks = ["0.0.0.0/0"]
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["${var.my_ip}"]
 	}
 }
 
--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@ variable "secret_key" {}
 variable "region" {
 	default = "us-east-1"
 }
+variable "my_ip" {
+    default = "184.54.89.91/32"
+}
